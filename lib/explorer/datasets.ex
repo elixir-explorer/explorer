3d34cca3bd664d8eed85181b9443da4dd3b672d8
Amanda Ferraboli <amanda.ferraboli@loft.com.br>
2021-11-17 23:05:18 -0300

    feat: adds wine dataset

--- lib/explorer/datasets.ex
+++ lib/explorer/datasets.ex
@@ -12,4 +12,24 @@ defmodule Explorer.Datasets do
   """
   def fossil_fuels,
     do: DataFrame.read_csv!("#{:code.priv_dir(:explorer)}/datasets/fossil_fuels.csv")
+
+  @doc """
+  Wine Dataset - The data is the result of a chemical analysis of wines grown in the same
+  region in Italy but derived from three different cultivars. The analysis determined the quantities
+  of 13 constituents found in each of the three types of wines.
+
+  Downloaded and modified from: https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data
+
+  ## Citation
+
+    Original Owners:
+    Forina, M. et al, PARVUS -
+    An Extendible Package for Data Exploration, Classification and Correlation.
+    Institute of Pharmaceutical and Food Analysis and Technologies, Via Brigata Salerno,
+    16147 Genoa, Italy.
+
+    Wine. (1991). UCI Machine Learning Repository.
+  """
+  def wine_data,
+    do: DataFrame.read_csv!("#{:code.priv_dir(:explorer)}/datasets/wine_data.csv")
 end
