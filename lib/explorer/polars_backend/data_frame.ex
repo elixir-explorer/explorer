950bf45ecd7b077aa906ba65a95dd48e5d376578
Christopher Grainger <chris@amplified.ai>
2022-03-23 16:03:45 +1100

    remove enum implementation on polars dataframe

--- lib/explorer/polars_backend/data_frame.ex
+++ lib/explorer/polars_backend/data_frame.ex
@@ -4,7 +4,6 @@ defmodule Explorer.PolarsBackend.DataFrame do
   alias __MODULE__, as: PolarsDF
   alias Explorer.DataFrame, as: DataFrame
   alias Explorer.PolarsBackend.Native
-  alias Explorer.PolarsBackend.LazyFrame
   alias Explorer.PolarsBackend.Series, as: PolarsSeries
   alias Explorer.PolarsBackend.Shared
   alias Explorer.Series, as: Series
@@ -155,21 +154,24 @@ defmodule Explorer.PolarsBackend.DataFrame do
   end
 
   @impl true
-  def to_map(%DataFrame{data: df}, convert_series?) do
-    Enum.reduce(df, %{}, &to_map_reducer(&1, &2, convert_series?))
-  end
+  def to_map(df, convert_series?) do
+    for series <- Shared.apply_native(df, :df_get_columns, [names(df)]), reduce: %{} do
+      acc ->
+        series_name =
+          series
+          |> Native.s_name()
+          |> then(fn {:ok, name} ->
+            String.to_atom(name)
+          end)
 
-  defp to_map_reducer(series, acc, convert_series?) do
-    series_name =
-      series
-      |> Native.s_name()
-      |> then(fn {:ok, name} ->
-        String.to_atom(name)
-      end)
+        series =
+          case convert_series? do
+            true -> series |> Shared.to_series() |> PolarsSeries.to_list()
+            false -> Shared.to_series(series)
+          end
 
-    series = Shared.to_series(series)
-    series = if convert_series?, do: PolarsSeries.to_list(series), else: series
-    Map.put(acc, series_name, series)
+        Map.put_new(acc, series_name, series)
+    end
   end
 
   # Introspection
@@ -207,28 +209,15 @@ defmodule Explorer.PolarsBackend.DataFrame do
   def head(%DataFrame{data: %PolarsDF{}} = df, rows),
     do: Shared.apply_native(df, :df_head, [rows])
 
-  def head(%DataFrame{data: %LazyFrame{}} = df, rows),
-    do: Shared.apply_native(df, :lf_head, [rows])
-
   @impl true
   def tail(df, rows), do: Shared.apply_native(df, :df_tail, [rows])
 
   @impl true
-  def select(%{data: %PolarsDF{}} = df, columns, :keep) when is_list(columns),
+  def select(df, columns, :keep) when is_list(columns),
     do: Shared.apply_native(df, :df_select, [columns])
 
-  def select(%{data: %LazyFrame{}} = df, columns, :keep) when is_list(columns),
-    do: Shared.apply_native(df, :lf_select, [columns])
-
-  def select(%{groups: groups} = df, columns, :drop) when is_list(columns),
-    do: df |> Shared.to_polars_df() |> drop(columns) |> Shared.to_dataframe(groups)
-
-  defp drop(polars_df, colnames),
-    do:
-      Enum.reduce(colnames, polars_df, fn name, df ->
-        {:ok, df} = Native.df_drop(df, name)
-        df
-      end)
+  def select(df, columns, :drop) when is_list(columns),
+    do: Shared.apply_native(df, :df_drop, [columns])
 
   @impl true
   def filter(df, %Series{} = mask),
@@ -429,48 +418,6 @@ defmodule Explorer.PolarsBackend.DataFrame do
     |> ungroup([])
     |> DataFrame.arrange(groups)
   end
-end
-
-defimpl Enumerable, for: Explorer.PolarsBackend.DataFrame do
-  alias Explorer.PolarsBackend.Native
-  alias Explorer.PolarsBackend.Series, as: PolarsSeries
-
-  def count(df), do: Native.df_width(df)
-
-  def slice(df) do
-    {:ok, size} = count(df)
-    {:ok, size, &slicing_fun(df, &1, &2)}
-  end
-
-  defp slicing_fun(df, start, length) do
-    for idx <- start..(start + length - 1) do
-      {:ok, df} = Native.df_select_at_idx(df, idx)
-      df
-    end
-  end
-
-  def reduce(_df, {:halt, acc}, _fun), do: {:halted, acc}
-  def reduce(df, {:suspend, acc}, fun), do: {:suspended, acc, &reduce(df, &1, fun)}
-
-  def reduce(df, {:cont, acc}, fun) do
-    case Native.df_columns(df) do
-      {:ok, []} ->
-        {:done, acc}
-
-      {:ok, [head | _tail]} ->
-        {:ok, next_col} = Native.df_column(df, head)
-        {:ok, df} = Native.df_drop(df, head)
-        reduce(df, fun.(next_col, acc), fun)
-    end
-  end
-
-  def member?(df, %PolarsSeries{} = series) do
-    {:ok, columns} = Native.df_get_columns(df)
-
-    {:ok, Enum.any?(columns, &Native.s_series_equal(&1, series, false))}
-  end
-
-  def member?(_, _), do: {:error, __MODULE__}
 end
 
 defimpl Inspect, for: Explorer.PolarsBackend.DataFrame do
