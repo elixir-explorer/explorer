caba36f0a5d4b4de5e1d22c4dbc90ec1b0c28435
kimjoaoun <joao.santos@comadre.com.br>
2022-04-10 19:57:56 -0300

    rust formatting

--- native/explorer/src/dataframe.rs
+++ native/explorer/src/dataframe.rs
@@ -151,7 +151,7 @@ pub fn df_to_csv_file(
 pub fn df_read_ipc(
     filename: &str,
     with_columns: Option<Vec<String>>,
-    with_projection: Option<Vec<usize>>
+    with_projection: Option<Vec<usize>>,
 ) -> Result<ExDataFrame, ExplorerError> {
     let f = File::open(filename)?;
     let df = IpcReader::new(f)
@@ -163,22 +163,22 @@ pub fn df_read_ipc(
 
 #[rustler::nif(schedule = "DirtyIo")]
 pub fn df_write_ipc(
-    data: ExDataFrame, 
+    data: ExDataFrame,
     filename: &str,
-    with_compression: Option<&str>) -> Result<(), ExplorerError> {
+    with_compression: Option<&str>,
+) -> Result<(), ExplorerError> {
     df_read!(data, df, {
-
         // Select the compression algorithm.
         let compression = match with_compression {
-            Some("LZ4") => {Some(IpcCompression::LZ4)},
-            Some("ZSTD") => {Some(IpcCompression::ZSTD)},
-            _ => {None},
+            Some("LZ4") => Some(IpcCompression::LZ4),
+            Some("ZSTD") => Some(IpcCompression::ZSTD),
+            _ => None,
         };
 
         let mut file = File::create(filename).expect("could not create file");
         IpcWriter::new(&mut file)
-        .with_compression(compression)
-        .finish(&mut df.clone())?;
+            .with_compression(compression)
+            .finish(&mut df.clone())?;
         Ok(())
     })
 }
