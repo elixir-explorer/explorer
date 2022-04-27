a54ada204e0f0b3ec6df16b18819561ec80b76b0
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-04-27 19:39:09 +0200

    Add tests for the enumerable

--- test/explorer/polars_backend/series_test.exs
+++ test/explorer/polars_backend/series_test.exs
@@ -50,4 +50,27 @@ defmodule Explorer.PolarsBackend.SeriesTest do
 
     assert Series.from_list(dates, :datetime) |> Series.to_list() == dates
   end
+
+  test "to_enum/1 returns a valid enumerable" do
+    enum1 =
+      [1, 2, 3, 4]
+      |> Explorer.Series.from_list(backend: Explorer.PolarsBackend)
+      |> Explorer.Series.to_enum()
+
+    enum2 =
+      ["a", "b", "c"]
+      |> Explorer.Series.from_list(backend: Explorer.PolarsBackend)
+      |> Explorer.Series.to_enum()
+
+    assert Enum.zip(enum1, enum2) == [{1, "a"}, {2, "b"}, {3, "c"}]
+
+    assert Enum.reduce(enum1, 0, &+/2) == 10
+    assert Enum.reduce(enum2, "", &<>/2) == "cba"
+
+    assert Enum.count(enum1) == 4
+    assert Enum.count(enum2) == 3
+
+    assert Enum.slice(enum1, 1..2) == [2, 3]
+    assert Enum.slice(enum2, 1..2) == ["b", "c"]
+  end
 end
