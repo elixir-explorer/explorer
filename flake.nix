948e9622cfe22c3be5d2ef755dd63d3b53eb8c38
Christopher Grainger <cigrainger@users.noreply.github.com>
2022-05-02 19:38:15 +1000

    add back simd (#198)
    * remove redundant module in docs
    
    * sort polars features
    
    * remove unnecessary strings feature
    
    * update flake for nightly toolchain
    
    * add 'performant' feature
    
    * add simd feature
    
    * update ci for nightly toolchain

--- flake.nix
+++ flake.nix
@@ -2,26 +2,40 @@
   description = "Explorer";
 
   inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
+      let
+        overlays = [ fenix.overlay ];
+        pkgs = import nixpkgs { inherit overlays system; };
+        toolchain = "latest";
+        rustPkg = fenix.packages.${system}.${toolchain}.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ];
+      in
+      {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             act
-            cargo
-            clippy
             elixir
             erlang
             gdb
-	    libiconv
+            libiconv
             openssl
             pkg-config
-            rustc
+            rustPkg
+            rust-analyzer-nightly
           ];
           shellHook = ''
             mkdir -p .nix-mix
