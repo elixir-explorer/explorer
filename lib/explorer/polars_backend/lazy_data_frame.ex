baccca94a051347168d06df704c6d64e088bca7d
Christopher Grainger <chris@amplified.ai>
2022-05-03 15:40:42 +1000

    raise for unimplemented functions

--- lib/explorer/polars_backend/lazy_data_frame.ex
+++ lib/explorer/polars_backend/lazy_data_frame.ex
@@ -2,4 +2,22 @@ defmodule Explorer.PolarsBackend.LazyDataFrame do
   @type t :: %__MODULE__{resource: binary(), reference: reference()}
 
   defstruct resource: nil, reference: nil
+
+  @behaviour Explorer.Backend.DataFrame
+
+  # TODO: Make the functions of non-implemented functions
+  # explicit once the lazy interface is ready.
+  funs =
+    Explorer.Backend.DataFrame.behaviour_info(:callbacks) --
+      (Explorer.Backend.DataFrame.behaviour_info(:optional_callbacks) ++
+         Module.definitions_in(__MODULE__, :def))
+
+  for {fun, arity} <- funs do
+    args = Macro.generate_arguments(arity, __MODULE__)
+
+    @impl true
+    def unquote(fun)(unquote_splicing(args)) do
+      raise "cannot perform operation on an Explorer.PolarsBackend.LazyDataFrame"
+    end
+  end
 end
