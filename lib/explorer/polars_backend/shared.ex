59992a2537c85d8c6c6a66f870d8de157dffe1c7
José Valim <jose.valim@dashbit.co>
2022-06-20 08:54:24 +0200

    Add TODO

--- lib/explorer/polars_backend/shared.ex
+++ lib/explorer/polars_backend/shared.ex
@@ -36,8 +36,6 @@ defmodule Explorer.PolarsBackend.Shared do
   def apply_dataframe(%DataFrame{} = df, %DataFrame{} = out_df, fun, args) do
     case apply(Native, fun, [df.data | args]) do
       {:ok, %module{} = new_df} when module in @polars_df -> %{out_df | data: new_df}
-      {:ok, %PolarsSeries{} = new_series} -> create_series(new_series)
-      {:ok, value} -> value
       {:error, error} -> raise "#{error}"
     end
   end
@@ -45,7 +43,6 @@ defmodule Explorer.PolarsBackend.Shared do
   def create_dataframe(%PolarsDataFrame{} = polars_df) do
     {:ok, names} = Native.df_columns(polars_df)
     {:ok, dtypes} = Native.df_dtypes(polars_df)
-
     dtypes = Enum.map(dtypes, &normalise_dtype/1)
 
     Explorer.Backend.DataFrame.new(polars_df, names, dtypes)
@@ -54,16 +51,17 @@ defmodule Explorer.PolarsBackend.Shared do
   def create_dataframe(%PolarsLazyFrame{} = polars_df) do
     {:ok, names} = Native.lf_names(polars_df)
     {:ok, dtypes} = Native.lf_dtypes(polars_df)
-
     dtypes = Enum.map(dtypes, &normalise_dtype/1)
 
     Explorer.Backend.DataFrame.new(polars_df, names, dtypes)
   end
 
-  # Updating is just creating a new DF with the same groups from existing DF
-  def update_dataframe(%module{} = polars_df, %DataFrame{} = df) when module in @polars_df do
+  # TODO: this currently assumes we want to preserve the groups
+  # but, if that's the case, we should be using apply_df/4.
+  # In other words, we should find every caller of this function
+  # and make it use apply_dataframe/4 instead.
+  defp update_dataframe(%module{} = polars_df, %DataFrame{} = df) when module in @polars_df do
     new_df = create_dataframe(polars_df)
-
     %{new_df | groups: df.groups}
   end
 
