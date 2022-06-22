afd1d28ac4db578fb2098e0c67cad40de988587f
Christopher Grainger <cigrainger@users.noreply.github.com>
2022-06-22 16:31:52 +1000

    revert groupby_stable (#256)
    * revert groupby_stable
    
    * ungroup before arranging grouped apply
    
    * remove extra arrange

--- lib/explorer/polars_backend/data_frame.ex
+++ lib/explorer/polars_backend/data_frame.ex
@@ -364,6 +364,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
       end)
     end)
     |> concat_rows()
+    |> DataFrame.ungroup()
     |> arrange([{:asc, idx_column}])
     |> select(out_df)
   end
@@ -476,11 +477,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
     columns =
       Enum.map(columns, fn {key, values} -> {key, Enum.map(values, &Atom.to_string/1)} end)
 
-    order = Enum.map(groups, &{:asc, &1})
-
-    df
-    |> Shared.apply_dataframe(out_df, :df_groupby_agg, [groups, columns])
-    |> arrange(order)
+    Shared.apply_dataframe(df, out_df, :df_groupby_agg, [groups, columns])
   end
 
   # Inspect
