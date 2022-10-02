8850ac2a2a39ffb7526f0881ece2872f27b49f40
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-10-02 10:17:06 +0200

    Prepare for draft PR

--- test/explorer/data_frame/csv_test.exs
+++ test/explorer/data_frame/csv_test.exs
@@ -48,39 +48,30 @@ defmodule Explorer.DataFrame.CSVTest do
     assert_in_delta(57.653484, frame["lat"][0], @f64_epsilon)
 
     city = frame["city"]
-    # TODO: write a meaningful test, but references are apparently changing
-    # assert city == frame[0]
 
     assert city[0] == "Elgin, Scotland, the UK"
     assert city[13] == "Aberdeen, Aberdeen City, UK"
   end
 
-  # TODO: expose what is in Explorer.Backend.Series @valid_dtypes attributes, so that
-  # we ensure the tests remain up to date?
-
-  # NOTE: just a preliminary batch of data, we may want to add more than just one value,
-  # with all covered cases.
-  @valid_dtypes [
-    {:integer, "100", 100},
-    {:float, "2.3", 2.3},
-    {:boolean, "true", true},
-    {:string, "some string", "some string"},
-    {:date, "2022-12-01", ~D[2022-12-01]},
-    {:datetime, "2022-10-01T11:34:10.123456", ~N[2022-10-01 11:34:10.123456]}
-    # Unsupported?
-    # :list
-  ]
-
-  describe "dtypes support" do
-    Enum.each(@valid_dtypes, fn {type, csv_value, parsed_value} ->
-      test "conforms to requested dtype for #{type}" do
+  describe "dtypes" do
+    [
+      {:integer, "100", 100},
+      {:float, "2.3", 2.3},
+      {:boolean, "true", true},
+      {:string, "some string", "some string"},
+      {:date, "2022-12-01", ~D[2022-12-01]},
+      {:datetime, "2022-10-01T11:34:10.123456", ~N[2022-10-01 11:34:10.123456]}
+      # :list (to be implemented)
+    ]
+    |> Enum.each(fn {type, csv_value, parsed_value} ->
+      test "supports explicit #{type} parsing" do
         data = "column\n#{unquote(csv_value)}"
         frame = DF.from_csv!(tmp_file!(data), dtypes: [{"column", unquote(type)}])
         assert frame[0][0] == unquote(Macro.escape(parsed_value))
         assert frame[0].dtype == unquote(type)
       end
 
-      test "infers requested dtype for #{type}" do
+      test "supports inferred #{type} parsing" do
         data = "column\n#{unquote(csv_value)}"
         frame = DF.from_csv!(tmp_file!(data), parse_dates: true)
         assert frame[0][0] == unquote(Macro.escape(parsed_value))
