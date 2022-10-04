aaff4d02fb33e6a9246642d023484127ac7734e0
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-10-04 11:56:24 +0200

    Add round-trip testing

--- test/explorer/data_frame/csv_test.exs
+++ test/explorer/data_frame/csv_test.exs
@@ -54,10 +54,13 @@ defmodule Explorer.DataFrame.CSVTest do
   end
 
   def assert_csv(type, csv_value, parsed_value, from_csv_options) do
-    data = "column\n#{csv_value}"
+    data = "column\n#{csv_value}\n"
+    # parsing should work as expected
     frame = DF.from_csv!(tmp_file!(data), from_csv_options)
     assert frame[0][0] == parsed_value
     assert frame[0].dtype == type
+    # but we also re-encode to make a full tested round-trip
+    assert DF.dump_csv(frame) == data
   end
 
   def assert_csv(type, csv_value, parsed_value) do
