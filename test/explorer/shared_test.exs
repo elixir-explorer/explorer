cd6839e931b44e70a78fe43c7266f645c0bc2b62
Jinkyou Son <nallwhy@gmail.com>
2022-10-15 00:21:05 +0900

    Add test for impl!/1 of Explorer.Shared

--- test/explorer/shared_test.exs
+++ test/explorer/shared_test.exs
@@ -11,6 +11,44 @@ defmodule Explorer.SharedTest do
     end
   end
 
+  describe "impl!/1" do
+    test "with a series" do
+      assert Shared.impl!(series()) == Explorer.PolarsBackend.Series
+    end
+
+    test "with a lazy series" do
+      assert Shared.impl!(lazy_series()) == Explorer.Backend.LazySeries
+    end
+
+    test "with list of series" do
+      assert Shared.impl!([series(), series()]) == Explorer.PolarsBackend.Series
+    end
+
+    test "with list of lazy series" do
+      assert Shared.impl!([lazy_series(), lazy_series()]) == Explorer.Backend.LazySeries
+    end
+
+    test "with list of series and lazy series" do
+      assert_raise RuntimeError, fn -> Shared.impl!([series(), lazy_series()]) end
+      assert_raise RuntimeError, fn -> Shared.impl!([lazy_series(), series()]) end
+    end
+  end
+
+  describe "impl!/2" do
+    test "with two series" do
+      assert Shared.impl!(series(), series()) == Explorer.PolarsBackend.Series
+    end
+
+    test "with two lazy series" do
+      assert Shared.impl!(lazy_series(), lazy_series()) == Explorer.Backend.LazySeries
+    end
+
+    test "with a series and a lazy series" do
+      assert_raise RuntimeError, fn -> Shared.impl!(series(), lazy_series()) end
+      assert_raise RuntimeError, fn -> Shared.impl!(lazy_series(), series()) end
+    end
+  end
+
   describe "apply_binary_op_impl/1" do
     test "applies when series is on the left-hand side" do
       :ok = Shared.apply_binary_op_impl(:ping, %{data: %FakeImpl{}}, 42)
@@ -34,5 +72,14 @@ defmodule Explorer.SharedTest do
         Shared.apply_binary_op_impl(:ping, 42, 13)
       end
     end
+  end
+
+  defp series(data \\ [1]) do
+    Explorer.Series.from_list(data, backend: Explorer.PolarsBackend)
+  end
+
+  defp lazy_series() do
+    data = Explorer.Backend.LazySeries.new(:column, ["col_a"])
+    Explorer.Backend.Series.new(data, :integer)
   end
 end
