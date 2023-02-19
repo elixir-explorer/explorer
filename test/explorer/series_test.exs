362c8dc6dbbff1d7e9f701a05e4d1b956e163af4
Jhonatan Nunes <jhonatannunessilva@hotmail.com>
2023-02-19 17:24:56 -0300

    Add Series.less/2 tests

--- test/explorer/series_test.exs
+++ test/explorer/series_test.exs
@@ -969,6 +969,20 @@ defmodule Explorer.SeriesTest do
       assert s1 |> Series.less(s2) |> Series.to_list() == [false, false, true]
     end
 
+    test "compare float series" do
+      s1 = Series.from_list([1.0, 2.0, :nan, :infinity, :neg_infinity, :neg_infinity])
+      s2 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity, :infinity])
+
+      assert s1 |> Series.less(s2) |> Series.to_list() == [
+               false,
+               true,
+               false,
+               false,
+               false,
+               true
+             ]
+    end
+
     test "compare time series" do
       s1 = Series.from_list([~T[00:00:00.000000], ~T[12:00:00.000000], ~T[23:59:59.999999]])
       s2 = Series.from_list([~T[00:00:00.000000], ~T[12:30:00.000000], ~T[23:50:59.999999]])
@@ -982,10 +996,106 @@ defmodule Explorer.SeriesTest do
       assert s1 |> Series.less(2) |> Series.to_list() == [true, true, false, false]
     end
 
+    test "compare float series with a float value on the right-hand side" do
+      s1 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity])
+
+      assert s1 |> Series.less(2.0) |> Series.to_list() == [
+               true,
+               false,
+               false,
+               false,
+               true
+             ]
+    end
+
+    test "compare float series with a nan value on the right-hand side" do
+      s1 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity])
+
+      assert s1 |> Series.less(:nan) |> Series.to_list() == [
+               false,
+               false,
+               false,
+               false,
+               false
+             ]
+    end
+
+    test "compare float series with an infinity value on the right-hand side" do
+      s1 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity])
+
+      assert s1 |> Series.less(:infinity) |> Series.to_list() == [
+               true,
+               true,
+               false,
+               false,
+               true
+             ]
+    end
+
+    test "compare float series with an negative infinity value on the right-hand side" do
+      s1 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity])
+
+      assert s1 |> Series.less(:neg_infinity) |> Series.to_list() == [
+               false,
+               false,
+               false,
+               false,
+               false
+             ]
+    end
+
     test "compare integer series with a scalar value on the left-hand side" do
       s1 = Series.from_list([1, 0, 2, 3])
 
       assert 2 |> Series.less(s1) |> Series.to_list() == [false, false, false, true]
+    end
+
+    test "compare float series with a float value on the left-hand side" do
+      s1 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity])
+
+      assert 2.5 |> Series.less(s1) |> Series.to_list() == [
+               false,
+               true,
+               false,
+               true,
+               false
+             ]
+    end
+
+    test "compare float series with a nan value on the left-hand side" do
+      s1 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity])
+
+      assert :nan |> Series.less(s1) |> Series.to_list() == [
+               false,
+               false,
+               false,
+               false,
+               false
+             ]
+    end
+
+    test "compare float series with an infinity value on the left-hand side" do
+      s1 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity])
+
+      assert :infinity |> Series.less(s1) |> Series.to_list() == [
+               false,
+               false,
+               false,
+               false,
+               false
+             ]
+    end
+
+    test "compare float series with an negative infinity value on the left-hand side" do
+      s1 = Series.from_list([1.0, 3.5, :nan, :infinity, :neg_infinity])
+
+      assert :neg_infinity |> Series.less(s1) |> Series.to_list() == [
+               true,
+               true,
+               false,
+               true,
+               false
+             ]
     end
   end
 
