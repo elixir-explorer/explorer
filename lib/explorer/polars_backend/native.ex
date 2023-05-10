b592ce84e8200d7616687562856b346da1a3ef9e
Philip Sampaio <philip.sampaio@gmail.com>
2023-05-10 17:36:54 -0300

    Disable precompilation for ARM 32 bits
    Also enable precompilation for OTP 26, which has a new NIF versiom,
    2.17. This is not a requirement, since the previous version should
    compatible.

--- lib/explorer/polars_backend/native.ex
+++ lib/explorer/polars_backend/native.ex
@@ -16,7 +16,6 @@ defmodule Explorer.PolarsBackend.Native do
       aarch64-apple-darwin
       aarch64-unknown-linux-gnu
       aarch64-unknown-linux-musl
-      arm-unknown-linux-gnueabihf
       riscv64gc-unknown-linux-gnu
       x86_64-apple-darwin
       x86_64-pc-windows-msvc
