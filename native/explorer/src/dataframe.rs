4af84d76c617bdcc0cf9e7071571dd13daa81791
Ahmad Sattar <thehabbos007@gmail.com>
2023-06-13 17:12:06 +0200

    Add errors and refactor (#613)

--- native/explorer/src/dataframe.rs
+++ native/explorer/src/dataframe.rs
@@ -286,38 +286,44 @@ pub fn df_sample_frac(
 #[rustler::nif]
 fn df_experiment(stream_ptr: u64, _ref: rustler::Term) -> Result<ExDataFrame, ExplorerError> {
     let stream_ptr = stream_ptr as *mut ffi::ArrowArrayStream;
-    match unsafe { stream_ptr.as_mut() } {
-        None => Err(ExplorerError::Other("Incorrect stream pointer".into())),
-        Some(stream_ref) => {
-            let mut res = unsafe { ffi::ArrowArrayStreamReader::try_new(stream_ref) }
-                .map_err(arrow_to_explorer_error)?;
+    let stream_ref = unsafe { stream_ptr.as_mut() }
+        .ok_or(ExplorerError::Other("Incorrect stream pointer".into()))?;
 
-            let df = match unsafe { res.next() } {
-                None => DataFrame::empty(),
-                Some(maybe) => {
-                    let mut acc = array_to_dataframe(maybe);
+    let mut res = unsafe { ffi::ArrowArrayStreamReader::try_new(stream_ref) }
+        .map_err(arrow_to_explorer_error)?;
 
-                    while let Some(maybe) = unsafe { res.next() } {
-                        let df = array_to_dataframe(maybe);
-                        acc.vstack_mut(&df)?;
-                    }
+    let df = match unsafe { res.next() } {
+        None => DataFrame::empty(),
+        Some(maybe) => {
+            let mut acc = array_to_dataframe(maybe)?;
 
-                    acc.rechunk();
-                    acc
-                }
-            };
+            while let Some(maybe) = unsafe { res.next() } {
+                let df = array_to_dataframe(maybe)?;
+                acc.vstack_mut(&df)?;
+            }
 
-            Ok(ExDataFrame::new(df))
+            acc.rechunk();
+            acc
         }
-    }
+    };
+
+    Ok(ExDataFrame::new(df))
 }
 
 fn array_to_dataframe(
-    maybe: Result<Box<dyn arrow::array::Array>, polars::error::ArrowError>,
-) -> DataFrame {
-    let ok = maybe.unwrap();
-    let sa = ok.as_any().downcast_ref::<StructArray>().unwrap();
-    DataFrame::try_from(sa.clone()).unwrap()
+    stream_chunk: Result<Box<dyn arrow::array::Array>, polars::error::ArrowError>,
+) -> Result<DataFrame, ExplorerError> {
+    let dyn_array = stream_chunk.map_err(arrow_to_explorer_error)?;
+
+    let struct_array = dyn_array
+        .as_any()
+        .downcast_ref::<StructArray>()
+        .ok_or(ExplorerError::Other(
+            "Unable to downcast to StructArray in ArrowArrayStreamReader chunk".into(),
+        ))?
+        .clone();
+
+    DataFrame::try_from(struct_array).map_err(ExplorerError::Polars)
 }
 
 fn arrow_to_explorer_error(error: impl std::fmt::Debug) -> ExplorerError {
