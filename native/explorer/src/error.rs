428014656ca90defe98f913a02a79b0730f17cc0
Jhonatan Nunes <44319738+Jhonatannunessilva@users.noreply.github.com>
2023-06-18 14:41:05 -0300

    Update the way Rust returns errors (#623)
    * Update encode function
    
    - After this change, all errors returned from Rust will be strings instead of tuples
    
    * Remove unused function

--- native/explorer/src/error.rs
+++ native/explorer/src/error.rs
@@ -1,4 +1,4 @@
-use rustler::{Atom, Encoder, Env, Term};
+use rustler::{Encoder, Env, Term};
 use std::io;
 use thiserror::Error;
 
@@ -16,15 +16,15 @@ rustler::atoms! {
 
 #[derive(Error, Debug)]
 pub enum ExplorerError {
-    #[error("IO Error")]
+    #[error("IO Error: {0}")]
     Io(#[from] io::Error),
-    #[error("Utf8 Conversion Error")]
+    #[error("Utf8 Conversion Error: {0}")]
     Utf8(#[from] std::string::FromUtf8Error),
-    #[error("Polars Error")]
+    #[error("Polars Error: {0}")]
     Polars(#[from] polars::prelude::PolarsError),
     #[error("Internal Error: {0}")]
     Internal(String),
-    #[error("Other error: {0}")]
+    #[error("Generic Error: {0}")]
     Other(String),
     #[error(transparent)]
     TryFromInt(#[from] std::num::TryFromIntError),
@@ -36,24 +36,6 @@ pub enum ExplorerError {
 
 impl Encoder for ExplorerError {
     fn encode<'b>(&self, env: Env<'b>) -> Term<'b> {
-        match self {
-            ExplorerError::Io(ref value) => error_tuple(env, io(), value.to_string()),
-            ExplorerError::Utf8(ref value) => error_tuple(env, utf8(), value.to_string()),
-            ExplorerError::Polars(ref value) => error_tuple(env, polars(), value.to_string()),
-
-            ExplorerError::Internal(ref value) => error_tuple(env, internal(), value.to_string()),
-
-            ExplorerError::Other(ref value) => error_tuple(env, other(), value.to_string()),
-            ExplorerError::TryFromInt(ref value) => {
-                error_tuple(env, try_from_int(), value.to_string())
-            }
-            ExplorerError::Parquet(ref value) => error_tuple(env, parquet(), value.to_string()),
-            ExplorerError::Unknown(ref value) => error_tuple(env, unknown(), value.to_string()),
-        }
+        format!("{self}").encode(env)
     }
-}
-
-// Encode an error tuple for better context at the Elixir side.
-fn error_tuple(env: Env, atom: Atom, error_str: String) -> Term {
-    (atom.encode(env), error_str.encode(env)).encode(env)
 }
