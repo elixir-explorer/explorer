48405aec62a87cfdfb0dacd2e480d44ab0a2735c
Philip Sampaio <philip.sampaio@gmail.com>
2023-08-02 17:51:58 -0300

    Add tests that cover file not found error

--- test/explorer/data_frame/ndjson_test.exs
+++ test/explorer/data_frame/ndjson_test.exs
@@ -210,7 +210,7 @@ defmodule Explorer.DataFrame.NDJSONTest do
     end
   end
 
-  describe "to_ndjson/3 - cloud" do
+  describe "cloud reads and writes" do
     setup do
       s3_config = %FSS.S3.Config{
         access_key_id: "test",
@@ -230,6 +230,13 @@ defmodule Explorer.DataFrame.NDJSONTest do
 
       saved_df = DF.from_ndjson!(path, config: s3_config)
       assert DF.to_columns(saved_df) == DF.to_columns(Explorer.Datasets.wine())
+    end
+
+    @tag :cloud_integration
+    test "returns an error in case file is not found in S3 bucket", %{s3_config: s3_config} do
+      path = "s3://test-bucket/test-writes/file-does-not-exist.ndjson"
+
+      assert {:error, "no such file or directory"} = DF.from_ndjson(path, config: s3_config)
     end
   end
 end
