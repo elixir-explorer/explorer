elixir-explorer/explorer

commit 9d17741f36f528e034f976eab23205c2299bd24e

Return errors as exception structs (#686)

lib/explorer/data_frame.ex
@@ -542,7 +542,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec from_csv(filename :: String.t() | fs_entry(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def from_csv(filename, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -623,7 +623,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec load_csv(contents :: String.t(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def load_csv(contents, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -694,7 +694,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec from_parquet(filename :: String.t() | fs_entry(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def from_parquet(filename, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -793,7 +793,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec to_parquet(df :: DataFrame.t(), filename :: String.t() | fs_entry(), opts :: Keyword.t()) ::
-          :ok | {:error, term()}
+          :ok | {:error, Exception.t()}
   def to_parquet(%DataFrame{} = df, filename, opts \\ []) do
     opts = Keyword.validate!(opts, compression: nil, streaming: true, config: nil)
     compression = parquet_compression(opts[:compression])
@@ -863,7 +863,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec dump_parquet(df :: DataFrame.t(), opts :: Keyword.t()) ::
-          {:ok, binary()} | {:error, term()}
+          {:ok, binary()} | {:error, Exception.t()}
   def dump_parquet(df, opts \\ []) do
     opts = Keyword.validate!(opts, compression: nil)
     compression = parquet_compression(opts[:compression])
@@ -888,7 +888,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec load_parquet(contents :: binary(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def load_parquet(contents, opts \\ []) do
     backend = backend_from_options!(opts)
     backend.load_parquet(contents)
@@ -926,7 +926,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec from_ipc(filename :: String.t() | fs_entry(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def from_ipc(filename, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -994,7 +994,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec to_ipc(df :: DataFrame.t(), filename :: String.t() | fs_entry(), opts :: Keyword.t()) ::
-          :ok | {:error, term()}
+          :ok | {:error, Exception.t()}
   def to_ipc(df, filename, opts \\ []) do
     opts = Keyword.validate!(opts, compression: nil, streaming: true, config: nil)
     compression = ipc_compression(opts[:compression])
@@ -1046,7 +1046,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec dump_ipc(df :: DataFrame.t(), opts :: Keyword.t()) ::
-          {:ok, binary()} | {:error, term()}
+          {:ok, binary()} | {:error, Exception.t()}
   def dump_ipc(df, opts \\ []) do
     opts = Keyword.validate!(opts, compression: nil)
     compression = ipc_compression(opts[:compression])
@@ -1079,7 +1079,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec load_ipc(contents :: binary(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def load_ipc(contents, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -1130,7 +1130,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec from_ipc_stream(filename :: String.t() | fs_entry()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def from_ipc_stream(filename, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -1186,7 +1186,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec to_ipc_stream(df :: DataFrame.t(), filename :: String.t() | fs_entry()) ::
-          :ok | {:error, term()}
+          :ok | {:error, Exception.t()}
   def to_ipc_stream(df, filename, opts \\ []) do
     opts = Keyword.validate!(opts, compression: nil, config: nil)
     compression = ipc_compression(opts[:compression])
@@ -1235,7 +1235,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec dump_ipc_stream(df :: DataFrame.t(), opts :: Keyword.t()) ::
-          {:ok, binary()} | {:error, term()}
+          {:ok, binary()} | {:error, Exception.t()}
   def dump_ipc_stream(df, opts \\ []) do
     opts = Keyword.validate!(opts, compression: nil)
     compression = ipc_compression(opts[:compression])
@@ -1268,7 +1268,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec load_ipc_stream(contents :: binary(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def load_ipc_stream(contents, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -1314,7 +1314,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec to_csv(df :: DataFrame.t(), filename :: fs_entry() | String.t(), opts :: Keyword.t()) ::
-          :ok | {:error, term()}
+          :ok | {:error, Exception.t()}
   def to_csv(df, filename, opts \\ []) do
     opts = Keyword.validate!(opts, header: true, delimiter: ",", config: nil)
 
@@ -1357,7 +1357,8 @@ defmodule Explorer.DataFrame do
       {:ok, "year,country,total,solid_fuel,liquid_fuel,gas_fuel,cement,gas_flaring,per_capita,bunker_fuels\\n2010,AFGHANISTAN,2308,627,1601,74,5,0,0.08,9\\n2010,ALBANIA,1254,117,953,7,177,0,0.43,7\\n"}
   """
   @doc type: :io
-  @spec dump_csv(df :: DataFrame.t(), opts :: Keyword.t()) :: {:ok, String.t()} | {:error, term()}
+  @spec dump_csv(df :: DataFrame.t(), opts :: Keyword.t()) ::
+          {:ok, String.t()} | {:error, Exception.t()}
   def dump_csv(df, opts \\ []) do
     opts = Keyword.validate!(opts, header: true, delimiter: ",")
     Shared.apply_impl(df, :dump_csv, [opts[:header], opts[:delimiter]])
@@ -1397,7 +1398,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec from_ndjson(filename :: String.t() | fs_entry(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def from_ndjson(filename, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -1454,7 +1455,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec to_ndjson(df :: DataFrame.t(), filename :: String.t() | fs_entry(), opts :: Keyword.t()) ::
-          :ok | {:error, term()}
+          :ok | {:error, Exception.t()}
   def to_ndjson(df, filename, opts \\ []) do
     opts = Keyword.validate!(opts, config: nil)
 
@@ -1495,7 +1496,7 @@ defmodule Explorer.DataFrame do
 
   """
   @doc type: :io
-  @spec dump_ndjson(df :: DataFrame.t()) :: {:ok, binary()} | {:error, term()}
+  @spec dump_ndjson(df :: DataFrame.t()) :: {:ok, binary()} | {:error, Exception.t()}
   def dump_ndjson(df) do
     Shared.apply_impl(df, :dump_ndjson, [])
   end
@@ -1531,7 +1532,7 @@ defmodule Explorer.DataFrame do
   """
   @doc type: :io
   @spec load_ndjson(contents :: String.t(), opts :: Keyword.t()) ::
-          {:ok, DataFrame.t()} | {:error, term()}
+          {:ok, DataFrame.t()} | {:error, Exception.t()}
   def load_ndjson(contents, opts \\ []) do
     {backend_opts, opts} = Keyword.split(opts, [:backend, :lazy])
 
@@ -1601,7 +1602,7 @@ defmodule Explorer.DataFrame do
   Collecting a grouped dataframe should return a grouped dataframe.
   """
   @doc type: :conversion
-  @spec collect(df :: DataFrame.t()) :: {:ok, DataFrame.t()} | {:error, term()}
+  @spec collect(df :: DataFrame.t()) :: DataFrame.t()
   def collect(df), do: Shared.apply_impl(df, :collect)
 
   @doc """

lib/explorer/fss/http.ex
@@ -9,8 +9,8 @@ defimpl Explorer.FSS, for: FSS.HTTP.Entry do
 
     case Utils.download(entry.url, collectable, headers: headers) do
       {:ok, _collectable} -> :ok
-      {:error, _message, 404} -> Utils.posix_error(:enoent)
-      {:error, message, _status} -> {:error, message}
+      {:error, _message, 404} -> {:error, ArgumentError.exception("resource not found (404)")}
+      {:error, exception, _status} -> {:error, exception}
     end
   end
 end

lib/explorer/fss/s3.ex
@@ -10,8 +10,8 @@ defimpl Explorer.FSS, for: FSS.S3.Entry do
 
     case Utils.download(url, collectable, headers: headers) do
       {:ok, _collectable} -> :ok
-      {:error, _message, 404} -> Utils.posix_error(:enoent)
-      {:error, message, _status} -> {:error, message}
+      {:error, _message, 404} -> {:error, ArgumentError.exception("resource not found (404)")}
+      {:error, exception, _status} -> {:error, exception}
     end
   end
 

lib/explorer/fss/utils.ex
@@ -26,15 +26,6 @@ defmodule Explorer.FSS.Utils do
   end
 
   @doc """
-  Converts the given posix error atom into readable error tuple.
-  """
-  @spec posix_error(atom()) :: {:error, String.t()}
-  def posix_error(error) do
-    message = error |> :file.format_error() |> List.to_string()
-    {:error, message}
-  end
-
-  @doc """
   Downloads resource at the given URL into `collectable`.
 
   If collectable raises and error, it is rescued and an error tuple
@@ -46,7 +37,7 @@ defmodule Explorer.FSS.Utils do
 
   """
   @spec download(String.t(), Collectable.t(), keyword()) ::
-          {:ok, Collectable.t()} | {:error, String.t(), status()}
+          {:ok, Collectable.t()} | {:error, Exception.t(), status()}
   def download(url, collectable, opts \\ []) do
     headers = build_headers(opts[:headers] || [])
 
@@ -86,7 +77,7 @@ defmodule Explorer.FSS.Utils do
           collector.(acc, :halt)
           :httpc.cancel_request(request_id)
           exception = Exception.normalize(kind, reason, __STACKTRACE__)
-          {:error, Exception.message(exception), nil}
+          {:error, exception, nil}
       else
         {:ok, state} ->
           acc = state.collector.(state.acc, :done)
@@ -95,13 +86,13 @@ defmodule Explorer.FSS.Utils do
         {:error, message, status} ->
           collector.(acc, :halt)
           :httpc.cancel_request(request_id)
-          {:error, message, status}
+          {:error, ArgumentError.exception(message), status}
       end
     catch
       kind, reason ->
         :httpc.cancel_request(request_id)
         exception = Exception.normalize(kind, reason, __STACKTRACE__)
-        {:error, Exception.message(exception), nil}
+        {:error, exception, nil}
     end
   end
 

lib/explorer/polars_backend/data_frame.ex
@@ -123,7 +123,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case df do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -150,7 +150,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case Native.df_to_csv(df, entry.path, header?, delimiter) do
       {:ok, _} -> :ok
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -160,13 +160,16 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case Native.df_to_csv_cloud(df, entry, header?, delimiter) do
       {:ok, _} -> :ok
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
   @impl true
   def dump_csv(%DataFrame{} = df, header?, <<delimiter::utf8>>) do
-    Native.df_dump_csv(df.data, header?, delimiter)
+    case Native.df_dump_csv(df.data, header?, delimiter) do
+      {:ok, string} -> {:ok, string}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
+    end
   end
 
   @impl true
@@ -216,7 +219,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case df do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -241,35 +244,41 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
   @impl true
   def from_ndjson(%Local.Entry{} = entry, infer_schema_length, batch_size) do
-    with {:ok, df} <- Native.df_from_ndjson(entry.path, infer_schema_length, batch_size) do
-      {:ok, Shared.create_dataframe(df)}
+    case Native.df_from_ndjson(entry.path, infer_schema_length, batch_size) do
+      {:ok, df} -> {:ok, Shared.create_dataframe(df)}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
   @impl true
   def to_ndjson(%DataFrame{data: df}, %Local.Entry{} = entry) do
-    with {:ok, _} <- Native.df_to_ndjson(df, entry.path) do
-      :ok
+    case Native.df_to_ndjson(df, entry.path) do
+      {:ok, _} -> :ok
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
   @impl true
   def to_ndjson(%DataFrame{data: df}, %S3.Entry{} = entry) do
-    with {:ok, _} <- Native.df_to_ndjson_cloud(df, entry) do
-      :ok
+    case Native.df_to_ndjson_cloud(df, entry) do
+      {:ok, _} -> :ok
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
   @impl true
   def dump_ndjson(%DataFrame{} = df) do
-    Native.df_dump_ndjson(df.data)
+    case Native.df_dump_ndjson(df.data) do
+      {:ok, string} -> {:ok, string}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
+    end
   end
 
   @impl true
   def load_ndjson(contents, infer_schema_length, batch_size) when is_binary(contents) do
     case Native.df_load_ndjson(contents, infer_schema_length, batch_size) do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -310,7 +319,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case df do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -323,7 +332,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
       ) do
     case Native.df_to_parquet(df, entry.path, parquet_compression(compression, compression_level)) do
       {:ok, _} -> :ok
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -340,13 +349,16 @@ defmodule Explorer.PolarsBackend.DataFrame do
            parquet_compression(compression, compression_level)
          ) do
       {:ok, _} -> :ok
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
   @impl true
   def dump_parquet(%DataFrame{data: df}, {compression, compression_level}) do
-    Native.df_dump_parquet(df, parquet_compression(compression, compression_level))
+    case Native.df_dump_parquet(df, parquet_compression(compression, compression_level)) do
+      {:ok, string} -> {:ok, string}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
+    end
   end
 
   defp parquet_compression(nil, _), do: :uncompressed
@@ -361,7 +373,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
   def load_parquet(contents) when is_binary(contents) do
     case Native.df_load_parquet(contents) do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -385,7 +397,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case Native.df_from_ipc(entry.path, columns, projection) do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -393,7 +405,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
   def to_ipc(%DataFrame{data: df}, %Local.Entry{} = entry, {compression, _level}, _streaming) do
     case Native.df_to_ipc(df, entry.path, maybe_atom_to_string(compression)) do
       {:ok, _} -> :ok
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -401,13 +413,16 @@ defmodule Explorer.PolarsBackend.DataFrame do
   def to_ipc(%DataFrame{data: df}, %S3.Entry{} = entry, {compression, _level}, _streaming) do
     case Native.df_to_ipc_cloud(df, entry, maybe_atom_to_string(compression)) do
       {:ok, _} -> :ok
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
   @impl true
   def dump_ipc(%DataFrame{data: df}, {compression, _level}) do
-    Native.df_dump_ipc(df, maybe_atom_to_string(compression))
+    case Native.df_dump_ipc(df, maybe_atom_to_string(compression)) do
+      {:ok, string} -> {:ok, string}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
+    end
   end
 
   @impl true
@@ -416,7 +431,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case Native.df_load_ipc(contents, columns, projection) do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -440,7 +455,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case Native.df_from_ipc_stream(entry.path, columns, projection) do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -448,7 +463,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
   def to_ipc_stream(%DataFrame{data: df}, %Local.Entry{} = entry, {compression, _level}) do
     case Native.df_to_ipc_stream(df, entry.path, maybe_atom_to_string(compression)) do
       {:ok, _} -> :ok
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -456,13 +471,16 @@ defmodule Explorer.PolarsBackend.DataFrame do
   def to_ipc_stream(%DataFrame{data: df}, %S3.Entry{} = entry, {compression, _level}) do
     case Native.df_to_ipc_stream_cloud(df, entry, maybe_atom_to_string(compression)) do
       {:ok, _} -> :ok
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
   @impl true
   def dump_ipc_stream(%DataFrame{data: df}, {compression, _level}) do
-    Native.df_dump_ipc_stream(df, maybe_atom_to_string(compression))
+    case Native.df_dump_ipc_stream(df, maybe_atom_to_string(compression)) do
+      {:ok, string} -> {:ok, string}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
+    end
   end
 
   @impl true
@@ -471,7 +489,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
     case Native.df_load_ipc_stream(contents, columns, projection) do
       {:ok, df} -> {:ok, Shared.create_dataframe(df)}
-      {:error, error} -> {:error, error}
+      {:error, error} -> {:error, RuntimeError.exception(error)}
     end
   end
 
@@ -624,18 +642,14 @@ defmodule Explorer.PolarsBackend.DataFrame do
 
   @impl true
   def describe(%DataFrame{} = df, percentiles) do
-    case Native.df_describe(df.data, percentiles) do
-      {:ok, polars_df} -> Shared.create_dataframe(polars_df)
-      {:error, error} -> raise "cannot describe dataframe. Reason: #{inspect(error)}"
-    end
+    Shared.apply(:df_describe, [df.data, percentiles])
+    |> Shared.create_dataframe()
   end
 
   @impl true
   def nil_count(%DataFrame{} = df) do
-    case Native.df_nil_count(df.data) do
-      {:ok, polars_df} -> Shared.create_dataframe(polars_df)
-      {:error, error} -> raise "cannot count nils. Reason: #{inspect(error)}"
-    end
+    Shared.apply(:df_nil_count, [df.data])
+    |> Shared.create_dataframe()
   end
 
   @impl true
@@ -734,19 +748,14 @@ defmodule Explorer.PolarsBackend.DataFrame do
   def pivot_wider(df, id_columns, names_from, values_from, names_prefix) do
     names_prefix_optional = unless names_prefix == "", do: names_prefix
 
-    case Native.df_pivot_wider(
-           df.data,
-           id_columns,
-           names_from,
-           values_from,
-           names_prefix_optional
-         ) do
-      {:ok, %__MODULE__{} = polars_df} ->
-        Shared.create_dataframe(polars_df)
-
-      {:error, error} ->
-        raise "cannot pivot wider due to Polars error: #{inspect(error)}"
-    end
+    Shared.apply(:df_pivot_wider, [
+      df.data,
+      id_columns,
+      names_from,
+      values_from,
+      names_prefix_optional
+    ])
+    |> Shared.create_dataframe()
   end
 
   # Two or more table verbs

commit d5faae0350897a947c15d8ccb6e393a40d35f46a

Return errors as exception structs for FSS integration
This is the remaining work that started in
https://github.com/elixir-explorer/explorer/pull/686

We are now using the FSS version from Hex.pm.

lib/explorer/data_frame.ex
@@ -250,7 +250,7 @@ defmodule Explorer.DataFrame do
   @typedoc """
   Represents a filesystem entry, that can be local or S3.
   """
-  @type fs_entry :: Local.Entry.t() | S3.Entry.t() | HTTP.Entry.t()
+  @type fs_entry :: FSS.entry()
 
   @typedoc """
   Represents a dataframe.
@@ -730,7 +730,7 @@ defmodule Explorer.DataFrame do
     S3.parse(entry, config: config)
   end
 
-  defp normalise_entry("file://" <> path, _config), do: {:ok, %Local.Entry{path: path}}
+  defp normalise_entry("file://" <> path, _config), do: {:ok, Local.from_path(path)}
 
   defp normalise_entry("http://" <> _rest = url, config) do
     HTTP.parse(url, config: config)
@@ -741,7 +741,7 @@ defmodule Explorer.DataFrame do
   end
 
   defp normalise_entry(filepath, _config) when is_binary(filepath) do
-    {:ok, %Local.Entry{path: filepath}}
+    {:ok, Local.from_path(filepath)}
   end
 
   @doc """

lib/explorer/fss.ex
@@ -4,6 +4,6 @@ defprotocol Explorer.FSS do
   @doc """
   Downloads a given entry to a path.
   """
-  @spec download(t(), path :: Path.t()) :: :ok | {:error, term()}
+  @spec download(t(), path :: Path.t()) :: :ok | {:error, Exception.t()}
   def download(entry, path)
 end

lib/explorer/fss/http.ex
@@ -2,15 +2,15 @@ defimpl Explorer.FSS, for: FSS.HTTP.Entry do
   alias Explorer.FSS.Utils
 
   def download(entry, path) do
-    Utils.assert_regular_path!(path)
+    with :ok <- Utils.assert_regular_path(path) do
+      headers = entry.config.headers
+      collectable = File.stream!(path)
 
-    headers = entry.config.headers
-    collectable = File.stream!(path)
-
-    case Utils.download(entry.url, collectable, headers: headers) do
-      {:ok, _collectable} -> :ok
-      {:error, _message, 404} -> {:error, ArgumentError.exception("resource not found (404)")}
-      {:error, exception, _status} -> {:error, exception}
+      case Utils.download(entry.url, collectable, headers: headers) do
+        {:ok, _collectable} -> :ok
+        {:error, _message, 404} -> {:error, ArgumentError.exception("resource not found (404)")}
+        {:error, exception, _status} -> {:error, exception}
+      end
     end
   end
 end

lib/explorer/fss/s3.ex
@@ -2,16 +2,16 @@ defimpl Explorer.FSS, for: FSS.S3.Entry do
   alias Explorer.FSS.Utils
 
   def download(entry, path) do
-    Utils.assert_regular_path!(path)
+    with :ok <- Utils.assert_regular_path(path),
+         {:ok, url} <- url(entry) do
+      headers = headers(entry, :get, url, [])
+      collectable = File.stream!(path)
 
-    url = url(entry)
-    headers = headers(entry, :get, url, [])
-    collectable = File.stream!(path)
-
-    case Utils.download(url, collectable, headers: headers) do
-      {:ok, _collectable} -> :ok
-      {:error, _message, 404} -> {:error, ArgumentError.exception("resource not found (404)")}
-      {:error, exception, _status} -> {:error, exception}
+      case Utils.download(url, collectable, headers: headers) do
+        {:ok, _collectable} -> :ok
+        {:error, _message, 404} -> {:error, ArgumentError.exception("resource not found (404)")}
+        {:error, exception, _status} -> {:error, exception}
+      end
     end
   end
 
@@ -20,29 +20,33 @@ defimpl Explorer.FSS, for: FSS.S3.Entry do
 
     uri = URI.parse(config.endpoint)
 
-    uri =
+    maybe_uri =
       if is_nil(config.bucket) do
-        uri
+        {:ok, uri}
       else
         append_path(uri, "/" <> config.bucket)
       end
 
-    uri
-    |> append_path("/" <> entry.key)
-    |> URI.to_string()
+    with {:ok, uri} <- maybe_uri,
+         {:ok, uri_with_key} <- append_path(uri, "/" <> entry.key) do
+      {:ok, URI.to_string(uri_with_key)}
+    end
   end
 
   # Once we depend on Elixir ~> 1.15, we can use `URI.append_path/2`.
   defp append_path(%URI{}, "//" <> _ = path) do
-    raise ArgumentError, ~s|path cannot start with "//", got: #{inspect(path)}|
+    {:error, ArgumentError.exception(~s|path cannot start with "//", got: #{inspect(path)}|)}
   end
 
   defp append_path(%URI{path: path} = uri, "/" <> rest = all) do
-    cond do
-      path == nil -> %{uri | path: all}
-      path != "" and :binary.last(path) == ?/ -> %{uri | path: path <> rest}
-      true -> %{uri | path: path <> all}
-    end
+    updated_uri =
+      cond do
+        path == nil -> %{uri | path: all}
+        path != "" and :binary.last(path) == ?/ -> %{uri | path: path <> rest}
+        true -> %{uri | path: path <> all}
+      end
+
+    {:ok, updated_uri}
   end
 
   defp headers(%FSS.S3.Entry{} = entry, method, url, headers, body \\ nil) do

lib/explorer/fss/utils.ex
@@ -7,14 +7,16 @@ defmodule Explorer.FSS.Utils do
 
   @doc """
   Asserts that the given path is a regular file path.
-  """
-  @spec assert_regular_path!(path()) :: :ok
-  def assert_regular_path!(path) do
-    unless regular_path?(path) do
-      raise ArgumentError, "expected a regular file path, got: #{inspect(path)}"
-    end
 
-    :ok
+  It returns an error in case the file is a directory.
+  """
+  @spec assert_regular_path(path()) :: :ok | {:error, Exception.t()}
+  def assert_regular_path(path) do
+    if regular_path?(path) do
+      :ok
+    else
+      {:error, ArgumentError.exception("expected a regular file path, got: #{inspect(path)}")}
+    end
   end
 
   @doc """

lib/explorer/polars_backend/data_frame.ex
@@ -53,7 +53,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
     path = Shared.build_path_for_entry(entry)
 
     with :ok <- Explorer.FSS.download(entry, path) do
-      entry = %Local.Entry{path: path}
+      entry = Local.from_path(path)
 
       result =
         from_csv(
@@ -233,7 +233,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
     path = Shared.build_path_for_entry(entry)
 
     with :ok <- Explorer.FSS.download(entry, path) do
-      entry = %Local.Entry{path: path}
+      entry = Local.from_path(path)
 
       result = from_ndjson(entry, infer_schema_length, batch_size)
 
@@ -296,7 +296,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
     path = Shared.build_path_for_entry(entry)
 
     with :ok <- Explorer.FSS.download(entry, path) do
-      entry = %Local.Entry{path: path}
+      entry = Local.from_path(path)
 
       result = from_parquet(entry, max_rows, columns)
 
@@ -382,7 +382,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
     path = Shared.build_path_for_entry(entry)
 
     with :ok <- Explorer.FSS.download(entry, path) do
-      entry = %Local.Entry{path: path}
+      entry = Local.from_path(path)
 
       result = from_ipc(entry, columns)
 
@@ -440,7 +440,7 @@ defmodule Explorer.PolarsBackend.DataFrame do
     path = Shared.build_path_for_entry(entry)
 
     with :ok <- Explorer.FSS.download(entry, path) do
-      entry = %Local.Entry{path: path}
+      entry = Local.from_path(path)
 
       result = from_ipc_stream(entry, columns)
 

mix.exs
@@ -38,7 +38,7 @@ defmodule Explorer.MixProject do
     [
       {:aws_signature, "~> 0.3"},
       {:castore, "~> 1.0"},
-      {:fss, github: "elixir-explorer/fss", branch: "main"},
+      {:fss, "~> 0.1"},
       {:rustler_precompiled, "~> 0.6"},
       {:table, "~> 0.1.2"},
       {:adbc, "~> 0.1", optional: true},