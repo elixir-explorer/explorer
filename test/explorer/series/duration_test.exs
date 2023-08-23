936f36a1d355e076433f51b0424bbb61d331bb3c
William Lanchantin <william.lanchantin@cargosense.com>
2023-08-23 09:48:37 -0400

    fix test after error type change

--- test/explorer/series/duration_test.exs
+++ test/explorer/series/duration_test.exs
@@ -192,19 +192,19 @@ defmodule Explorer.Series.DurationTest do
       assert Series.to_list(quot_s) == [@one_hour_us / 10]
     end
 
-    test "integer / duration[μs] raises RuntimeError" do
+    test "integer / duration[μs] raises ArgumentError" do
       ten_s = Series.from_list([10])
       one_hour_s = Series.from_list([@one_hour_us], dtype: {:duration, :microsecond})
 
-      assert_raise RuntimeError,
+      assert_raise ArgumentError,
                    "cannot divide by duration",
                    fn -> Series.divide(ten_s, one_hour_s) end
     end
 
-    test "duration[μs] / duration[μs] raises RuntimeError" do
+    test "duration[μs] / duration[μs] raises ArgumentError" do
       one_hour_s = Series.from_list([@one_hour_us], dtype: {:duration, :microsecond})
 
-      assert_raise RuntimeError,
+      assert_raise ArgumentError,
                    "cannot divide by duration",
                    fn -> Series.divide(one_hour_s, one_hour_s) end
     end
