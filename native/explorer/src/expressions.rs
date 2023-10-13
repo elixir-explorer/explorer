a042ab1492a6db544e107fb3ff906547e0639b1b
Matthew O'Neill <oneill_91@hotmail.co.uk>
2023-10-13 23:07:17 +0100

    add naive initial replace expression
    might not work with expressions

--- native/explorer/src/expressions.rs
+++ native/explorer/src/expressions.rs
@@ -795,6 +795,15 @@ pub fn expr_substring(expr: ExExpr, offset: i64, length: Option<u64>) -> ExExpr 
     let expr = expr.clone_inner();
     ExExpr::new(expr.str().str_slice(offset, length))
 }
+#[rustler::nif]
+pub fn expr_replace(expr: ExExpr, pat: String, value: String) -> ExExpr {
+    let expr = expr.clone_inner();
+    ExExpr::new(expr.str().replace_all(
+        Expr::Literal(LiteralValue::Utf8(pat)),
+        Expr::Literal(LiteralValue::Utf8(value)),
+        true,
+    ))
+}
 
 #[rustler::nif]
 pub fn expr_round(expr: ExExpr, decimals: u32) -> ExExpr {
