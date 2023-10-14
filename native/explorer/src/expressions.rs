ac0301ff1a5660e4375a801deda3820a9a7c382f
José Valim <jose.valim@gmail.com>
2023-10-14 09:29:16 +0200

    Update native/explorer/src/expressions.rs

--- native/explorer/src/expressions.rs
+++ native/explorer/src/expressions.rs
@@ -795,6 +795,7 @@ pub fn expr_substring(expr: ExExpr, offset: i64, length: Option<u64>) -> ExExpr 
     let expr = expr.clone_inner();
     ExExpr::new(expr.str().str_slice(offset, length))
 }
+
 #[rustler::nif]
 pub fn expr_replace(expr: ExExpr, pat: String, value: String) -> ExExpr {
     let expr = expr.clone_inner();
