9ea91ab0cfaeee6d333354c0822ab2d79d410760
Philip Sampaio <philip.sampaio@gmail.com>
2023-12-29 13:38:35 -0300

    Rename functions to follow our signed int aliases

--- native/explorer/src/series.rs
+++ native/explorer/src/series.rs
@@ -31,10 +31,10 @@ macro_rules! from_list {
     };
 }
 
-from_list!(s_from_list_i8, i8);
-from_list!(s_from_list_i16, i16);
-from_list!(s_from_list_i32, i32);
-from_list!(s_from_list_i64, i64);
+from_list!(s_from_list_s8, i8);
+from_list!(s_from_list_s16, i16);
+from_list!(s_from_list_s32, i32);
+from_list!(s_from_list_s64, i64);
 
 from_list!(s_from_list_u8, u8);
 from_list!(s_from_list_u16, u16);
@@ -213,10 +213,10 @@ macro_rules! from_binary {
 from_binary!(s_from_binary_f32, f32, 4);
 from_binary!(s_from_binary_f64, f64, 8);
 
-from_binary!(s_from_binary_i8, i8, 1);
-from_binary!(s_from_binary_i16, i16, 2);
-from_binary!(s_from_binary_i32, i32, 4);
-from_binary!(s_from_binary_i64, i64, 8);
+from_binary!(s_from_binary_s8, i8, 1);
+from_binary!(s_from_binary_s16, i16, 2);
+from_binary!(s_from_binary_s32, i32, 4);
+from_binary!(s_from_binary_s64, i64, 8);
 
 from_binary!(s_from_binary_u8, u8, 1);
 from_binary!(s_from_binary_u16, u16, 2);
