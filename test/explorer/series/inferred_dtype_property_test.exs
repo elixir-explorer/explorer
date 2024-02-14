a78b283acbc7bdf495e3ae13b287033a7cbe21db
William Lanchantin <william.lanchantin@cargosense.com>
2024-02-14 12:46:50 -0500

    import StreamData for readability

--- test/explorer/series/inferred_dtype_property_test.exs
+++ test/explorer/series/inferred_dtype_property_test.exs
@@ -12,6 +12,8 @@ defmodule Explorer.Series.InferredDtypePropertyTest do
   use ExUnit.Case, async: true
   use ExUnitProperties
 
+  import StreamData
+
   alias Explorer.Series
 
   @moduletag timeout: :infinity
@@ -28,26 +30,20 @@ defmodule Explorer.Series.InferredDtypePropertyTest do
   end
 
   defp dtype_generator do
-    scalar_dtype_generator = StreamData.constant({:s, 64})
+    scalar_dtype_generator = constant({:s, 64})
 
     # We don't need complicated keys: single letter strings should suffice.
-    key_generator = StreamData.string(?a..?z, min_length: 1, max_length: 1)
+    key_generator = string(?a..?z, min_length: 1, max_length: 1)
 
     dtype_generator =
-      StreamData.tree(scalar_dtype_generator, fn generator ->
-        StreamData.one_of([
-          StreamData.tuple({
-            StreamData.constant(:list),
-            generator
-          }),
-          StreamData.tuple({
-            StreamData.constant(:struct),
-            StreamData.map(
-              StreamData.nonempty(StreamData.map_of(key_generator, generator, max_length: 3)),
-              # Building the list from a map then ensures unique keys.
-              &Enum.to_list/1
-            )
-          })
+      tree(scalar_dtype_generator, fn generator ->
+        # Building the keyword list from a map ensures unique keys.
+        keyword_generator =
+          map(nonempty(map_of(key_generator, generator, max_length: 3)), &Enum.to_list/1)
+
+        one_of([
+          tuple({constant(:list), generator}),
+          tuple({constant(:struct), keyword_generator})
         ])
       end)
 
@@ -57,21 +53,21 @@ defmodule Explorer.Series.InferredDtypePropertyTest do
   defp series_of_dtype_generator(dtype) do
     series_value_generator = build_series_value_generator(dtype)
 
-    StreamData.bind(StreamData.list_of(series_value_generator, max_length: 3), fn series_values ->
-      StreamData.constant(Explorer.Series.from_list(series_values))
+    bind(list_of(series_value_generator, max_length: 3), fn series_values ->
+      constant(Explorer.Series.from_list(series_values))
     end)
   end
 
   defp build_series_value_generator({:s, 64}),
-    do: StreamData.integer()
+    do: integer()
 
   defp build_series_value_generator({:list, dtype}),
-    do: StreamData.list_of(build_series_value_generator(dtype), max_length: 3)
+    do: list_of(build_series_value_generator(dtype), max_length: 3)
 
   defp build_series_value_generator({:struct, keyword_of_dtypes}) do
     keyword_of_dtypes
     |> Map.new(fn {key, dtype} -> {key, build_series_value_generator(dtype)} end)
-    |> StreamData.fixed_map()
+    |> fixed_map()
   end
 
   # The idea behind a "sub" dtype is that in the dtype tree, you can replace
