84153d3fe1211456fdfb6cace5962758cb079dcd
José Valim <jose.valim@dashbit.co>
2024-06-24 23:29:56 +0200

    Address map key ordering in tests

--- test/explorer/data_frame_test.exs
+++ test/explorer/data_frame_test.exs
@@ -708,7 +708,7 @@ defmodule Explorer.DataFrameTest do
     test "with map " do
       df = DF.new(%{a: [1, nil, 3], b: ["a", "b", nil]})
       df1 = DF.mutate(df, c: %{a: a, b: b, lit: 1, null: is_nil(a)})
-      assert df1.names == ["a", "b", "c"]
+      assert df1.names |> Enum.sort() == ["a", "b", "c"]
 
       assert df1.dtypes == %{
                "a" => {:s, 64},
@@ -732,7 +732,7 @@ defmodule Explorer.DataFrameTest do
     test "with map nested" do
       df = DF.new(%{a: [1, nil, 3], b: ["a", "b", nil]})
       df1 = DF.mutate(df, c: %{s: %{a: a, b: b}})
-      assert df1.names == ["a", "b", "c"]
+      assert df1.names |> Enum.sort() == ["a", "b", "c"]
 
       assert df1.dtypes == %{
                "a" => {:s, 64},
