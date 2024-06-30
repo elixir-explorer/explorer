f2f57e1eed733af3c91ec743f91551678bd97b29
Benedikt Reinartz <filmor@gmail.com>
2024-06-30 21:46:40 +0200

    Implement message-on-gc NIFs in Rust

--- native/explorer/src/local_message.rs
+++ native/explorer/src/local_message.rs
@@ -1,29 +1,51 @@
-use rustler::wrapper::{NIF_ENV, NIF_TERM};
-use rustler::{Env, Term};
-use std::ffi::c_void;
+use std::sync::Mutex;
 
-extern "C" {
-    pub fn local_message_open_resource(env: NIF_ENV) -> c_void;
-    fn local_message_on_gc(env: NIF_ENV, arg1: NIF_TERM, arg2: NIF_TERM) -> NIF_TERM;
-    fn is_local_message_when_gc(env: NIF_ENV, arg1: NIF_TERM) -> NIF_TERM;
+use rustler::env::SavedTerm;
+use rustler::{Env, LocalPid, OwnedEnv, ResourceArc, Term};
+
+pub struct LocalMessage {
+    pid: LocalPid,
+    env: Mutex<OwnedEnv>,
+    term: Mutex<SavedTerm>,
 }
 
-#[rustler::nif]
-pub fn message_on_gc<'a>(env: Env<'a>, pid: Term<'a>, term: Term<'a>) -> Term<'a> {
-    unsafe {
-        Term::new(
-            env,
-            local_message_on_gc(env.as_c_arg(), pid.as_c_arg(), term.as_c_arg()),
-        )
+unsafe impl Sync for LocalMessage {}
+
+impl LocalMessage {
+    pub fn new(pid: LocalPid, term: Term<'_>) -> Self {
+        let env = OwnedEnv::new();
+        let term = env.save(term);
+        Self {
+            pid,
+            env: Mutex::new(env),
+            term: Mutex::new(term),
+        }
+    }
+}
+
+impl Drop for LocalMessage {
+    fn drop(&mut self) {
+        // Since these mutexes are never locked by anything but this drop, both `try_lock`s should
+        // always succeed.
+        if let Ok(mut env) = self.env.try_lock() {
+            if let Ok(term) = self.term.try_lock() {
+                // If the send fails, the other process is gone.
+                env.run(|env| {
+                    let term = term.load(env);
+                    let _ = env.send(&self.pid, term);
+                });
+                let _ = env.send_and_clear(&self.pid, |env: Env| term.load(env));
+            }
+        }
     }
 }
 
 #[rustler::nif]
-pub fn is_message_on_gc<'a>(env: Env<'a>, term: Term<'a>) -> Term<'a> {
-    unsafe {
-        Term::new(
-            env,
-            is_local_message_when_gc(env.as_c_arg(), term.as_c_arg()),
-        )
-    }
+pub fn message_on_gc(pid: LocalPid, term: Term<'_>) -> ResourceArc<LocalMessage> {
+    ResourceArc::new(LocalMessage::new(pid, term))
+}
+
+#[rustler::nif]
+pub fn is_message_on_gc(term: Term<'_>) -> bool {
+    term.decode::<ResourceArc<LocalMessage>>().is_ok()
 }
