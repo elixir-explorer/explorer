784a6ce3afd02ff1052448411aa4d8a3b9ee1441
Benedikt Reinartz <filmor@gmail.com>
2024-07-09 22:58:28 +0200

    Remove mutex usage

--- native/explorer/src/local_message.rs
+++ native/explorer/src/local_message.rs
@@ -1,39 +1,33 @@
-use std::sync::Mutex;
-
 use rustler::env::SavedTerm;
 use rustler::{Env, LocalPid, OwnedEnv, Resource, ResourceArc, Term};
 
 pub struct LocalMessage {
     pid: LocalPid,
-    env_and_term: Mutex<(OwnedEnv, SavedTerm)>,
+    env: OwnedEnv,
+    term: SavedTerm,
 }
 
 impl LocalMessage {
     pub fn new(pid: LocalPid, term: Term<'_>) -> Self {
         let env = OwnedEnv::new();
         let term = env.save(term);
-        Self {
-            pid,
-            env_and_term: Mutex::new((env, term)),
-        }
+        Self { pid, env, term }
     }
 }
+
+// This is only safe because we only use the object for its destructor
+unsafe impl Sync for LocalMessage {}
 
 #[rustler::resource_impl]
 impl Resource for LocalMessage {
     fn destructor(self, env: Env) {
-        // Since this mutex is never locked by anything but this destructor, the `try_lock` should
-        // always succeed.
-        if let Ok(guard) = self.env_and_term.try_lock() {
-            let owned_env = &guard.0;
-            let term = &guard.1;
+        let owned_env = self.env;
 
-            // If the send fails, the other process is gone.
-            owned_env.run(|owned_env| {
-                let term = term.load(owned_env);
-                let _ = env.send(&self.pid, term);
-            });
-        }
+        // If the send fails, the other process is gone.
+        owned_env.run(|owned_env| {
+            let term = self.term.load(owned_env);
+            let _ = env.send(&self.pid, term);
+        });
     }
 }
 
