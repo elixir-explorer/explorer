72b18e59d908cf64f9d723c498f3a88675e47abc
Benedikt Reinartz <filmor@gmail.com>
2024-07-09 18:36:25 +0200

    Update to upcoming Rustler v0.34

--- native/explorer/src/local_message.rs
+++ native/explorer/src/local_message.rs
@@ -1,15 +1,12 @@
 use std::sync::Mutex;
 
 use rustler::env::SavedTerm;
-use rustler::{Env, LocalPid, OwnedEnv, ResourceArc, Term};
+use rustler::{Env, LocalPid, OwnedEnv, Resource, ResourceArc, Term};
 
 pub struct LocalMessage {
     pid: LocalPid,
-    env: Mutex<OwnedEnv>,
-    term: Mutex<SavedTerm>,
+    env_and_term: Mutex<(OwnedEnv, SavedTerm)>,
 }
-
-unsafe impl Sync for LocalMessage {}
 
 impl LocalMessage {
     pub fn new(pid: LocalPid, term: Term<'_>) -> Self {
@@ -17,32 +14,32 @@ impl LocalMessage {
         let term = env.save(term);
         Self {
             pid,
-            env: Mutex::new(env),
-            term: Mutex::new(term),
+            env_and_term: Mutex::new((env, term)),
         }
     }
 }
 
-impl Drop for LocalMessage {
-    fn drop(&mut self) {
-        // Since these mutexes are never locked by anything but this drop, both `try_lock`s should
+#[rustler::resource_impl]
+impl Resource for LocalMessage {
+    fn destructor(self, env: Env) {
+        // Since this mutex is never locked by anything but this destructor, the `try_lock` should
         // always succeed.
-        if let Ok(mut env) = self.env.try_lock() {
-            if let Ok(term) = self.term.try_lock() {
-                // If the send fails, the other process is gone.
-                env.run(|env| {
-                    let term = term.load(env);
-                    let _ = env.send(&self.pid, term);
-                });
-                let _ = env.send_and_clear(&self.pid, |env: Env| term.load(env));
-            }
+        if let Ok(guard) = self.env_and_term.try_lock() {
+            let owned_env = &guard.0;
+            let term = &guard.1;
+
+            // If the send fails, the other process is gone.
+            owned_env.run(|owned_env| {
+                let term = term.load(owned_env);
+                let _ = env.send(&self.pid, term);
+            });
         }
     }
 }
 
 #[rustler::nif]
 pub fn message_on_gc(pid: LocalPid, term: Term<'_>) -> ResourceArc<LocalMessage> {
-    ResourceArc::new(LocalMessage::new(pid, term))
+    LocalMessage::new(pid, term).into()
 }
 
 #[rustler::nif]
