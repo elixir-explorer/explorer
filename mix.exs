65b01e399208c71ce5e0370f6c3b47469eca6bc5
Philip Sampaio <philip.sampaio@gmail.com>
2024-08-27 11:50:52 -0300

    Start dev version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Explorer.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/elixir-nx/explorer"
-  @version "0.9.2"
+  @version "0.10.0-dev"
   @dev? String.ends_with?(@version, "-dev")
   @force_build? System.get_env("EXPLORER_BUILD") in ["1", "true"]
 
