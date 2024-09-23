667c19593875e486c2ce9ccae0dd161681d09367
William Lanchantin <william.lanchantin@cargosense.com>
2024-09-23 19:14:17 -0400

    Revert changes to `series.ex`

--- lib/explorer/series.ex
+++ lib/explorer/series.ex
@@ -1714,15 +1714,12 @@ defmodule Explorer.Series do
       >
   """
   @doc type: :element_wise
-  defmacro filter(series, series_query) do
+  defmacro filter(series, query) do
     quote do
       require Explorer.Query
 
-      df = Explorer.DataFrame.new(_: unquote(series))
-      df_query = Explorer.Query.query(Explorer.Backend.LazyFrame.new(df), unquote(series_query))
-
-      df
-      |> Explorer.DataFrame.filter_with(df_query)
+      Explorer.DataFrame.new(_: unquote(series))
+      |> Explorer.DataFrame.filter_with(Explorer.Query.query(unquote(query)))
       |> Explorer.DataFrame.pull(:_)
     end
   end
