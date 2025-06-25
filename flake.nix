e0ee746d2b4a5c9c1f10388fe14a62ad478f44c9
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-06-25 11:16:55 -0400

    chore(flake): update flake
    Updates the flake to bring in a more recently nightly compiler.
    
    Also, formats and removes unused variables

--- flake.nix
+++ flake.nix
@@ -10,23 +10,31 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ fenix.overlays.default ];
-        pkgs = import nixpkgs { inherit overlays system; };
-        toolchain = "latest";
-        rustPkg = fenix.packages.${system}.${toolchain}.withComponents [
-          "cargo"
-          "clippy"
-          "rust-src"
-          "rustc"
-          "rustfmt"
-        ];
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    fenix,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [fenix.overlays.default];
+      pkgs = import nixpkgs {inherit overlays system;};
+      rustToolchain = pkgs.fenix.fromToolchainName {
+        name = (nixpkgs.lib.importTOML ./rust-toolchain.toml).toolchain.channel;
+        sha256 = "sha256-UAoZcxg3iWtS+2n8TFNfANFt/GmkuOMDf7QAE0fRxeA=";
+      };
+
+      rustPkg = rustToolchain.withComponents [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
+      ];
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [
             act
             clang
             elixir
@@ -37,19 +45,20 @@
             rustPkg
             rust-analyzer-nightly
             cmake
-          ] ++ lib.optionals stdenv.isDarwin [
+          ]
+          ++ lib.optionals stdenv.isDarwin [
             darwin.apple_sdk.frameworks.Security
             darwin.apple_sdk.frameworks.SystemConfiguration
           ];
-          shellHook = ''
-            mkdir -p .nix-mix
-            mkdir -p .nix-hex
-            export MIX_HOME=$PWD/.nix-mix
-            export HEX_HOME=$PWD/.nix-hex
-            export PATH=$MIX_HOME/bin:$PATH
-            export PATH=$HEX_HOME/bin:$PATH
-            export PATH=$MIX_HOME/escripts:$PATH
-          '';
-        };
-      });
+        shellHook = ''
+          mkdir -p .nix-mix
+          mkdir -p .nix-hex
+          export MIX_HOME=$PWD/.nix-mix
+          export HEX_HOME=$PWD/.nix-hex
+          export PATH=$MIX_HOME/bin:$PATH
+          export PATH=$HEX_HOME/bin:$PATH
+          export PATH=$MIX_HOME/escripts:$PATH
+        '';
+      };
+    });
 }
