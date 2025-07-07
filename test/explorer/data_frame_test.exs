e9cb8bb1aaf3bcab538d7aeacce58e0d2a591821
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-07-07 11:37:05 -0400

    test: remove property

--- test/explorer/data_frame_test.exs
+++ test/explorer/data_frame_test.exs
@@ -4855,16 +4855,6 @@ defmodule Explorer.DataFrameTest do
       end
     end
 
-    property "can create any dataframe" do
-      check all(
-              dtypes <- Explorer.Generator.dtypes(),
-              rows <- Explorer.Generator.rows(dtypes),
-              max_runs: 1_000
-            ) do
-        assert %DF{} = DF.new(rows, dtypes: dtypes)
-      end
-    end
-
     property "can dump any DataFrame to IPC" do
       check all(
               # TODO: Remove `exclude: :category` after #1011 is resolved.
