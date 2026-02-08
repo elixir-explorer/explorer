54713c98834fa1344864504ad51834a6eb61be57
William Lanchantin <william.lanchantin@cargosense.com>
2026-02-08 12:43:55 -0500

    Revert "Upgrade Rustler to 37"
    This reverts commit 33c1c22dd329a035208c58b24f39d04ca3da7e4a.

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Explorer.MixProject do
       ## Optional
       {:adbc, "~> 0.1", optional: true},
       {:nx, "~> 0.4", optional: true},
-      {:rustler, "~> 0.37", optional: not (@dev? or @force_build?)},
+      {:rustler, "~> 0.36.0", optional: not (@dev? or @force_build?)},
 
       ## Test
       {:bypass, "~> 2.1", only: :test},
